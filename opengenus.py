f2dbf52dd3e98ca5e2817de9044131db725623d8
Vartika Yadav <38458783+vartikayadav@users.noreply.github.com>
2019-04-25 00:25:36 +0530

    Update opengenus.py
    updated file content

--- opengenus.py
+++ opengenus.py
@@ -1,12 +1,17 @@
 import requests
+import json
 print("Enter url of project")
 url=input()
 #print(url)
 x=url.split("https://github.com/")[1]
 info="https://api.github.com/repos/%s"%x
-print(info)
+#print(info)
 r=requests.get(info).json()
-print(r)
+#print((r))
 with open("data.json","w") as output:
-    output.write(str(r))
-
+    json.dump(r,output)
+with open("data.json","r") as output2:
+    data=json.load(output2)
+print(data)    
+print("forks_count: ",data["source"]["forks_count"])
+print("forks: ",data["source"]["forks"]) 
