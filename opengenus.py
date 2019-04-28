bce52bcb9d6be8d748ce961eba0188c2c2fb200a
Vartika Yadav <38458783+vartikayadav@users.noreply.github.com>
2019-04-28 22:16:59 +0530

    Update opengenus.py
    added client id and secretclient and made changes

--- opengenus.py
+++ opengenus.py
@@ -4,7 +4,7 @@ print("Enter url of project")
 url=input()
 #print(url)
 x=url.split("https://github.com/")[1]
-info="https://api.github.com/repos/%s"%x
+info="https://api.github.com/repos/%s?client_id=3e1b331062fb84d354a1&client_secret=3fc60c537530938bcae819e91fd97c59f246cbd7"%x
 #print(info)
 r=requests.get(info).json()
 #print((r))
@@ -13,5 +13,5 @@ with open("data.json","w") as output:
 with open("data.json","r") as output2:
     data=json.load(output2)
 print(data)    
-print("forks_count: ",data["source"]["forks_count"])
-print("forks: ",data["source"]["forks"]) 
+print("forks_count: ",data["forks_count"])
+print("forks: ",data["forks"]) 
